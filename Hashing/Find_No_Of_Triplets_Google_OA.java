533b62cb0ccfc7a6b54a36dd7cca47d7d43dcfd0
PSesharao <paritalasesharao@gmail.com>
2025-06-29 19:03:18 +0530

    find number of triplets on Sun, Jun 29, 2025  07:03:19 PM

--- Hashing/Find_No_Of_Triplets_Google_OA.java
+++ Hashing/Find_No_Of_Triplets_Google_OA.java
@@ -23,6 +23,40 @@ class JavaMain {
     private static FastReader in;
     private static FastWriter out ;
 
+    // 0 based indexing 
+    public static int findNumberOfTriplets( int arr[] ){
+
+        int N = arr.length ;  
+        if(N == 0 || N== 1 || N== 2)
+            return 0 ; 
+        int ans = 0 ;
+        int prefix[] = new int[N] ;  
+        int suffix[] = new int[N] ;  
+        Arrays.fill(prefix , 0 ) ; 
+        Arrays.fill(suffix , 0 ) ; 
+
+        // A[i] > A[j] -- prefix 
+        for(int j=1 ; j<N-1 ; j++ ){
+            for(int i=0 ; i<j ; i++ )
+                if( arr[i] > arr[j] )
+                    prefix[j]++ ; 
+
+        }
+
+        // A[j] < A[k] -- suffix
+        for(int j=1 ; j<N-1 ; j++ ){
+            for(int k=j+1 ; k<N ; k++)
+                if(arr[j] < arr[k])
+                    suffix[j]++ ; 
+        }
+
+        for(int j=1 ; j<N-1 ; j++ ){
+            ans += prefix[j] * suffix[j] ; 
+        }
+
+        return ans ; 
+    } 
+
     public static int findNumberOfTriplets( int arr[] )  {
         int N = arr.length-1 ; 
         int pref[] = new int[N+1]; 
