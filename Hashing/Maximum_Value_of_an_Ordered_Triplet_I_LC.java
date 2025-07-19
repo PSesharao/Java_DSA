fb8cea5f9a15940e0fad911fa05b48931aa664f1
PSesharao <paritalasesharao@gmail.com>
2025-07-20 01:15:42 +0530

    Max value of ordered triplet on Sun, Jul 20, 2025  01:15:43 AM

--- Hashing/Maximum_Value_of_an_Ordered_Triplet_I_LC.java
+++ Hashing/Maximum_Value_of_an_Ordered_Triplet_I_LC.java
@@ -1,6 +1,6 @@
 // https://leetcode.com/contest/weekly-contest-365/problems/maximum-value-of-an-ordered-triplet-i/
 // https://leetcode.com/contest/weekly-contest-365/problems/maximum-value-of-an-ordered-triplet-ii/
-
+// https://docs.google.com/document/d/10mhea8Ojo3maAkVpYeTKle3sanTz5ESSdXtYPo0p0Fg/edit?tab=t.0
 
 import java.io.*;
 import java.util.*;
@@ -11,44 +11,16 @@ import java.util.stream.* ;
 
 class Solution {
 
-    int countKDifference(int[] arr, int k) {
-
-        int n = arr.length ; 
-        
-        Map<Integer , Integer> map = new HashMap<>() ;
-
-        int count = 0 ; 
-
-        for(int i=0 ; i<n ; i++ ){
-
-            int curr = arr[i] ; 
-            int complement1 = curr - k ; 
-            int complement2 = curr + k ;  
-
-            // If the complement exists , we can form that many pairs 
-            // with the current element to complement.
-
-            count += map.getOrDefault(complement1 , 0 ) ;  
-
-            count += map.getOrDefault(complement2 , 0 ) ;  
-
-            int cur_freq = map.getOrDefault(curr , 0 ) ; 
-            map.put( curr , cur_freq + 1 ) ; 
-
-        }
-
-        return count ;  
-    }
-
 }
 
 class JavaMain {
 
-    private static FastReader in;
+    private static FastReader in ;
     private static FastWriter out ;
+    private static final int minVal = Integer.MIN_VALUE + 10 ; 
+    private static final int maxVal = Integer.MAX_VALUE - 10 ;
 
-
-    public long maximumTripletValue(int[] nums) {
+    public static long maximumTripletValue(int[] nums) {
         
         long ans = 0 ; 
         int N = nums.length ; 
@@ -71,16 +43,40 @@ class JavaMain {
     }
 
 
+    private static int maximumTripletValue_( int A[] ) {
+
+        int N = A.length ; 
+        if(N < 3)
+            return 0 ; 
+
+        int prefix[] = new int[N] ; 
+        Arrays.fill( prefix , 0 ) ;
+        prefix[1] = A[0] - A[1] ; 
+
+        for(int x=2 ; x<N ; x++ ){
+            int pair_diff = minVal ;
+            for(int y=0 ; y<x ; y++)
+                pair_diff = Math.max( pair_diff , A[y]-A[x] ) ; 
+            prefix[x] = Math.max( prefix[x-1] , pair_diff ) ; 
+        }
+
+        int ans = minVal ; 
+
+        for(int k=2 ; k<N ; k++) {
+            ans = Math.max(ans , prefix[k-1]*A[k] ) ; 
+        }
+
+        return ans ;      
+
+    }
+
+
 
     public static void main(String[] args) throws IOException  {
         initialize(); 
-        Solution sol = new Solution() ; 
-        
-        int arr[] = {1 , 4 , 1 , 3 , 2 , 2 } ;  
-        int k = 5 , n = 6 ; 
-
-        int ans = sol.getPairsCount2(arr , n , k) ;
-        out.println(ans) ; 
+        int arr[] = { 1,10,3,4,19 } ;
+        long ans = maximumTripletValue(arr) ; 
+        out.println(ans) ; // 133
         close() ; 
     }
 
